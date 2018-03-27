f4586bee85d605ea1652825ee7a36fdbeb7c3cdf
Abdel Ahd <ahdproduction@gmail.com>
2018-03-27 15:46:58 +0200

    Fill market with MarketItem array

--- src/app/market-view/market-view.component.ts
+++ src/app/market-view/market-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marketitem } from './marketitem';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -7,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./market-view.component.css']
 })
 export class MarketViewComponent implements OnInit {
-
+  marketItems: Marketitem[] = [];
   constructor() { }
 
   ngOnInit() {
+    for (let index = 0; index < 100; index++) {
+      this.marketItems.push(
+        new Marketitem(index, 'name', 'assets/img/ca.jpg', 100, false)
+      );
+      console.log('element #', this.marketItems[index].Id);
+    }
   }
 
 }
---
+++ src/app/market-view/marketitem.ts
@@ -0,0 +1,9 @@
+export class Marketitem {
+    constructor (
+        public Id: number,
+        public name: string,
+        public img: string,
+        public price: number,
+        public isHidden: boolean,
+    ) { }
+}
