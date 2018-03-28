240ac528abf668a74fa6873db1fbd93ae82d5047
Abdel Ahd <ahdproduction@gmail.com>
2018-03-28 10:13:18 +0200

    Show params in alert

--- src/app/market-view/market-view.component.ts
+++ src/app/market-view/market-view.component.ts
@@ -2,25 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Marketitem } from './market-item';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'market-view',
-  templateUrl: './market-view.component.html',
-  styleUrls: ['./market-view.component.css']
+	// tslint:disable-next-line:component-selector
+	selector: 'market-view',
+	templateUrl: './market-view.component.html',
+	styleUrls: ['./market-view.component.css']
 })
 export class MarketViewComponent implements OnInit {
 
-  marketItems: Marketitem[] = [];
+	marketItems: Marketitem[] = [];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-    for (let index = 0; index < 100; index++) {
-      this.marketItems.push(
-        new Marketitem(index, 'name', 'assets/img/ca.jpg', 100, false)
-      );
-      console.log('element #', this.marketItems[index].Id);
-    }
+	ngOnInit() {
+		for (let index = 0; index < 100; index++) {
+			this.marketItems.push(
+				new Marketitem(index, 'name', 'assets/img/ca.jpg', 100, false)
+			);
+			// console.log('element #', this.marketItems[index].Id);
+		}
+	}
 
-  }
-
+	open(item: Marketitem): void {
+		window.alert(item.Id);
+	}
 }
